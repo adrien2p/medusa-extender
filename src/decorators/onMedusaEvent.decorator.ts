0b7ddc940f8e61b4273913f3a96307afa864736a
adrien2p <adrien.deperetti@gmail.com>
2022-01-26 14:57:40 +0100

    Feat() Export new components

--- src/decorators/onMedusaEvent.decorator.ts
+++ src/decorators/onMedusaEvent.decorator.ts
@@ -35,66 +35,66 @@ export type MedusaEventHandlerParams<Entity, TEntityActions extends EntityAction
 };
 
 export class OnMedusaEvent {
-    readonly #when: string;
-    #targetEntity: any;
+	readonly #when: string;
+	#targetEntity: any;
 
-    constructor(when?: string) {
-        this.#when = when;
-    }
+	constructor(when?: string) {
+		this.#when = when;
+	}
 
-    static get Before(): OnMedusaEvent {
-        return this.build('Before');
-    }
+	static get Before(): OnMedusaEvent {
+		return this.build('Before');
+	}
 
-    static get After(): OnMedusaEvent {
-        return this.build('After');
-    }
+	static get After(): OnMedusaEvent {
+		return this.build('After');
+	}
 
-    private static build(when: string): OnMedusaEvent {
-        return new OnMedusaEvent(when);
-    }
+	private static build(when: string): OnMedusaEvent {
+		return new OnMedusaEvent(when);
+	}
 
-    public InsertEvent<Entity>(entity: Constructor<Entity>): string {
-        return `${this.#when}Insert${entity.name}`;
-    }
+	public InsertEvent<Entity>(entity: Constructor<Entity>): string {
+		return `${this.#when}Insert${entity.name}`;
+	}
 
-    public UpdateEvent<Entity>(entity: Constructor<Entity>): string {
-        return `${this.#when}Update${entity.name}`;
-    }
+	public UpdateEvent<Entity>(entity: Constructor<Entity>): string {
+		return `${this.#when}Update${entity.name}`;
+	}
 
-    public RemoveEvent<Entity>(entity: Constructor<Entity>): string {
-        return `${this.#when}Remove${entity.name}`;
-    }
+	public RemoveEvent<Entity>(entity: Constructor<Entity>): string {
+		return `${this.#when}Remove${entity.name}`;
+	}
 
-    public Insert<TEntity extends typeof Entity>(
-        entity: TEntity,
-        options: EntityEventActionOptions<TEntity> = { async: false }
-    ): MethodDecorator {
-        return this.buildDecorator('Insert', entity, options);
-    }
+	public Insert<TEntity extends typeof Entity>(
+		entity: TEntity,
+		options: EntityEventActionOptions<TEntity> = { async: false }
+	): MethodDecorator {
+		return this.buildDecorator('Insert', entity, options);
+	}
 
-    public Update<TEntity extends typeof Entity>(
-        entity: TEntity,
-        options: EntityEventActionOptions<TEntity> = { async: false }
-    ): MethodDecorator {
-        return this.buildDecorator('Update', entity, options);
-    }
+	public Update<TEntity extends typeof Entity>(
+		entity: TEntity,
+		options: EntityEventActionOptions<TEntity> = { async: false }
+	): MethodDecorator {
+		return this.buildDecorator('Update', entity, options);
+	}
 
-    public Remove<TEntity extends typeof Entity>(
-        entity: TEntity,
-        options: EntityEventActionOptions<TEntity> = { async: false }
-    ): MethodDecorator {
-        return this.buildDecorator('Remove', entity, options);
-    }
+	public Remove<TEntity extends typeof Entity>(
+		entity: TEntity,
+		options: EntityEventActionOptions<TEntity> = { async: false }
+	): MethodDecorator {
+		return this.buildDecorator('Remove', entity, options);
+	}
 
-    private buildDecorator<TEntity extends typeof Entity>(
-        action: EntityActions,
-        entity: TEntity,
-        options: EntityEventActionOptions<TEntity> = { async: false }
-    ) {
-        this.#targetEntity = entity;
-        return OnMedusaEntityEventDecorator(`${this.#when}${action}${entity.name}`, entity, options);
-    }
+	private buildDecorator<TEntity extends typeof Entity>(
+		action: EntityActions,
+		entity: TEntity,
+		options: EntityEventActionOptions<TEntity> = { async: false }
+	) {
+		this.#targetEntity = entity;
+		return OnMedusaEntityEventDecorator(`${this.#when}${action}${entity.name}`, entity, options);
+	}
 }
 
 /**
@@ -105,36 +105,36 @@ export class OnMedusaEvent {
  * @param customMetatype The key that represent the class in the container it belongs to (Used to resolve the real instance)
  */
 function OnMedusaEntityEventDecorator<TEntity extends typeof Entity>(
-    eventName: string,
-    targetEntity: TEntity,
-    { async, metatype }: { async: boolean; metatype?: Type } = {
-        async: false,
-    }
+	eventName: string,
+	targetEntity: TEntity,
+	{ async, metatype }: { async: boolean; metatype?: Type } = {
+		async: false,
+	}
 ): MethodDecorator {
-    return (target: Type, propertyKey: string, descriptor: PropertyDescriptor): void => {
-        const original = descriptor.value;
-        descriptor.value = async function <Entity>({
-                                                       values,
-                                                       resolveOrReject,
-                                                   }: MedusaEventEmittedParams<Entity, any>): Promise<void> {
-            if (!(values.event.entity instanceof targetEntity)) {
-                return;
-            }
+	return (target: Type, propertyKey: string, descriptor: PropertyDescriptor): void => {
+		const original = descriptor.value;
+		descriptor.value = async function <Entity>({
+			values,
+			resolveOrReject,
+		}: MedusaEventEmittedParams<Entity, any>): Promise<void> {
+			if (!(values.event.entity instanceof targetEntity)) {
+				return;
+			}
 
-            const promise = original.apply(this, [values]);
-            if (async) {
-                return promise
-                    .then((res: unknown) => {
-                        return resolveOrReject(null, res);
-                    })
-                    .catch((err: Error) => {
-                        return resolveOrReject(err);
-                    });
-            } else {
-                return resolveOrReject();
-            }
-        };
+			const promise = original.apply(this, [values]);
+			if (async) {
+				return promise
+					.then((res: unknown) => {
+						return resolveOrReject(null, res);
+					})
+					.catch((err: Error) => {
+						return resolveOrReject(err);
+					});
+			} else {
+				return resolveOrReject();
+			}
+		};
 
-        customEventEmitter.register(eventName, propertyKey, metatype ?? target);
-    };
+		customEventEmitter.register(eventName, propertyKey, metatype ?? target);
+	};
 }
--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,12 @@ export { customEventEmitter as EventEmitter } from './event-emmiter';
 export * from './types';
 
 export * from './decorators/onMedusaEvent.decorator';
+export * from './decorators/injectable.decorator';
+export * from './decorators/module.decorator';
 
 import { Utils as _Utils } from './utils';
 
 export const Utils = {
-    attachOrReplaceEntitySubscriber: _Utils.attachOrReplaceEntitySubscriber,
-    repositoryMixin: _Utils.repositoryMixin,
+	attachOrReplaceEntitySubscriber: _Utils.attachOrReplaceEntitySubscriber,
+	repositoryMixin: _Utils.repositoryMixin,
 };
