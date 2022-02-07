f0c8d22aa2a68e1cf59b39e2b572a387a0cda262
adrien2p <adrien.deperetti@gmail.com>
2022-02-07 18:40:51 +0100

    fix: Remove scope from entity

--- src/types.ts
+++ src/types.ts
@@ -17,7 +17,6 @@ export type InjectableComponentTypes = 'entity' | 'repository' | 'service' | 'mi
  * Defines the injection options for entities.
  */
 export type EntityInjectableOptions<TOverride = unknown> = {
-	scope?: LifetimeType;
 	resolutionKey?: string;
 	override?: Type<TOverride>;
 };
