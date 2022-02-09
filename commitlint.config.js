b8c11a3127f6981ea4b27de7965b78221d3d7e33
adrien2p <adrien.deperetti@gmail.com>
2022-02-09 17:51:32 +0100

    fix: deduplicate subject-empty property from commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -6,7 +6,6 @@ module.exports = {
 		'body-max-line-length': [2, 'always', 100],
 		'footer-max-line-length': [2, 'always', 100],
 		'header-max-length': [2, 'always', 100],
-		'subject-case': [2, 'never'],
 		'subject-empty': [2, 'never'],
 		'subject-case': [2, 'never'],
 		'subject-full-stop': [2, 'never', '.'],
