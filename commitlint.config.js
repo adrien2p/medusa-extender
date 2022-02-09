3e042bb677e88a001e4efa036fe24c88a986cc41
adrien2p <adrien.deperetti@gmail.com>
2022-02-09 13:34:08 +0100

    fix(docs): Fix images and expandable not displaying properly

--- commitlint.config.js
+++ commitlint.config.js
@@ -6,6 +6,7 @@ module.exports = {
 		'footer-max-line-length': [2, 'always', 100],
 		'header-max-length': [2, 'always', 100],
 		'subject-empty': [2, 'never'],
+		'subject-case': [2, 'never'],
 		'subject-full-stop': [2, 'never', '.'],
 		'type-case': [2, 'always', 'lower-case'],
 		'type-empty': [2, 'never'],
