cc3c8f935f90467c86e93d6f0b5276a2073a6ca1
adrien2p <adrien.deperetti@gmail.com>
2022-03-08 18:44:58 +0100

    feat: Allow to pass modules along side the components to a @Module

--- src/core/metadata-reader.ts
+++ src/core/metadata-reader.ts
@@ -2,39 +2,52 @@ import { GetInjectableOptions, InjectableComponentTypes, InjectableOptions, Type
 import { INJECTABLE_OPTIONS_KEY, MODULE_KEY } from './constants';
 
 class CustomMap extends Map<InjectableComponentTypes, GetInjectableOptions> {
-	get<TComponentType extends InjectableComponentTypes>(key: TComponentType): GetInjectableOptions<TComponentType> {
-		return super.get(key) as GetInjectableOptions<TComponentType>;
-	}
-}
-
-/**
- * Read all metadata from the imported modules and extract components that will be stored by there type.
- * @param modules The modules from which the metadata are read.
- */
-export function metadataReader(modules: Type[]): CustomMap {
-	const optionsMap = new CustomMap();
-
-	for (const module of modules) {
-		const moduleImports = Reflect.getMetadata(MODULE_KEY, module);
-
-		for (const component of moduleImports) {
-			const options = componentsMetadataReader(component);
-			const metatype = component;
-
-			optionsMap.set(options.type, [...(optionsMap.get(options.type) ?? []), { ...options, metatype }]);
-		}
-	}
-
-	return optionsMap;
+    get<TComponentType extends InjectableComponentTypes>(key: TComponentType): GetInjectableOptions<TComponentType> {
+        return super.get(key) as GetInjectableOptions<TComponentType>;
+    }
 }
 
 /**
  * @Internal
+ *
+ * Read all metadata from the imported modules and extract components that will be stored by there type.
+ * @param modules The modules from which the metadata are read.
+ */
+export function metadataReader(modules: Type[]): CustomMap {
+    let componentMap = new CustomMap();
+
+    for (const module of modules) {
+        const moduleImports = Reflect.getMetadata(MODULE_KEY, module);
+
+        for (const component of moduleImports) {
+            const subModuleImports = Reflect.getMetadata(MODULE_KEY, component);
+            if (subModuleImports) {
+                const subComponentMap = metadataReader([component]);
+                componentMap = new CustomMap([
+                    ...Array.from(componentMap.entries()),
+                    ...Array.from(subComponentMap.entries())
+                ]);
+                continue;
+            }
+
+            const options = componentsMetadataReader(component);
+            const metatype = component;
+
+            componentMap.set(options.type, [...(componentMap.get(options.type) ?? []), { ...options, metatype }]);
+        }
+    }
+
+    return componentMap;
+}
+
+/**
+ * @Internal
+ *
  * Return the options from components.
  * @param component
  */
 export function componentsMetadataReader<TComponentType = unknown>(
-	component: Type
+    component: Type
 ): InjectableOptions<TComponentType> & { type: InjectableComponentTypes } {
-	return Reflect.getMetadata(INJECTABLE_OPTIONS_KEY, component);
+    return Reflect.getMetadata(INJECTABLE_OPTIONS_KEY, component);
 }
---
+++ src/core/tests/metadata-reader.spec.ts
@@ -0,0 +1,44 @@
+import { metadataReader } from "../metadata-reader";
+import { Middleware, Module, Service } from "../../decorators";
+
+@Middleware({ requireAuth: false, routes: [] })
+export class FakeMiddleware {}
+
+@Service({ resolutionKey: 'FakeService' })
+export class FakeService {}
+
+@Middleware({ requireAuth: false, routes: [] })
+export class FakeMiddleware2 {}
+
+@Service({ resolutionKey: 'FakeService2' })
+export class FakeService2 {}
+
+@Module({ imports: [FakeMiddleware2, FakeService2] })
+export class FakeSubModule {}
+
+@Module({ imports: [FakeSubModule, FakeMiddleware, FakeService] })
+export class FakeModule {}
+
+describe('metadata-reader', () => {
+    it('should create a map of components from the modules', () => {
+        const componentsMap = metadataReader([FakeModule]);
+
+        expect(Array.from(componentsMap.entries()).length).toBe(2);
+        expect(componentsMap.get('middleware').length).toBe(2);
+        expect(componentsMap.get('service').length).toBe(2);
+
+        const allComponents = Array.from(componentsMap.values()).reduce((components, values) => {
+            components.push(...values);
+            return components;
+        }, []);
+        expect(allComponents.length).toBe(4);
+
+        const middlewareNames = componentsMap.get('middleware').map(component => component.metatype.name);
+        expect(middlewareNames.includes('FakeMiddleware')).toBeTruthy();
+        expect(middlewareNames.includes('FakeMiddleware2')).toBeTruthy();
+
+        const serviceNames = componentsMap.get('service').map(component => component.metatype.name);
+        expect(serviceNames.includes('FakeService')).toBeTruthy();
+        expect(serviceNames.includes('FakeService2')).toBeTruthy();
+    });
+});
