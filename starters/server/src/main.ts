61be5f9b298386a0bffd245f6d2a50e3a006b7eb
adrien2p <adrien.deperetti@gmail.com>
2022-03-16 15:26:02 +0100

    chore(starters): lint

--- starters/server/src/main.ts
+++ starters/server/src/main.ts
@@ -7,9 +7,7 @@ import { UserModule } from './modules/user/user.module';
 async function bootstrap() {
 	const expressInstance = express();
 
-	await new Medusa(resolve(__dirname, '..'), expressInstance).load([
-		UserModule
-	]);
+	await new Medusa(resolve(__dirname, '..'), expressInstance).load([UserModule]);
 
 	expressInstance.listen(config.serverConfig.port, () => {
 		console.info('Server successfully started on port ' + config.serverConfig.port);
