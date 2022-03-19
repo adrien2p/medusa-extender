127a093e610fce409ab5a7623c36eb5bd9c31a90
adrien2p <adrien.deperetti@gmail.com>
2022-03-19 21:58:41 +0100

    refactor(cli): Update router template to include the next function argument

--- src/cli/templates/router-template.ts
+++ src/cli/templates/router-template.ts
@@ -8,7 +8,7 @@ export function getRouterTemplate(routerName: string): string {
 	return dedent`
         import { MedusaAuthenticatedRequest, Router } from 'medusa-extender';
         import { UserService } from "@medusajs/medusa/dist/services";
-        import { Response } from "express";
+        import { Response, NextFunction } from "express";
         import { User } from "@medusajs/medusa/dist";
         
         @Router({
@@ -17,7 +17,7 @@ export function getRouterTemplate(routerName: string): string {
                 path: '/admin/custom-route',
                 method: 'get',
                 handlers: [
-                    async (req: MedusaAuthenticatedRequest, res: Response): Promise<Response<User[]>> => {
+                    async (req: MedusaAuthenticatedRequest, res: Response, next: NextFunction): Promise<Response<User[]>> => {
                         /* You can create a function in a separate find and just imported it here. */
                         const userService = req.scope.resolve('userService') as UserService;
                         const users = await userService.list({})
