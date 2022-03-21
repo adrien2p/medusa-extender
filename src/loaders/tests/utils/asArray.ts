2f8942b45f4b63f6bd7797179ed4231132ef1c7a
adrien2p <adrien.deperetti@gmail.com>
2022-03-21 13:11:32 +0100

    style: lint

--- src/loaders/tests/utils/asArray.ts
+++ src/loaders/tests/utils/asArray.ts
@@ -1,8 +1,10 @@
 import { AwilixContainer } from 'awilix';
-import { Resolver } from "awilix/lib/resolvers";
-import { ClassOrFunctionReturning } from "awilix/lib/container";
+import { Resolver } from 'awilix/lib/resolvers';
+import { ClassOrFunctionReturning } from 'awilix/lib/container';
 
-export function asArray<T>(resolvers: (ClassOrFunctionReturning<T> | Resolver<T>)[]): { resolve: (container: AwilixContainer, opts: Record<string, unknown>) => T[] } {
+export function asArray<T>(resolvers: (ClassOrFunctionReturning<T> | Resolver<T>)[]): {
+	resolve: (container: AwilixContainer, opts: Record<string, unknown>) => T[];
+} {
 	return {
 		resolve: (container, opts) => resolvers.map((r) => container.build(r, opts)),
 	};
