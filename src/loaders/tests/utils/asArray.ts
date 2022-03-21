751485e55aec524eb7cb45f60248751853de88a8
adrien2p <adrien.deperetti@gmail.com>
2022-03-21 12:49:55 +0100

    style: Fix typings

--- src/loaders/tests/utils/asArray.ts
+++ src/loaders/tests/utils/asArray.ts
@@ -1,6 +1,8 @@
 import { AwilixContainer } from 'awilix';
+import { Resolver } from "awilix/lib/resolvers";
+import { ClassOrFunctionReturning } from "awilix/lib/container";
 
-export function asArray(resolvers): { resolve: <T>(container: AwilixContainer, opts: Record<string, unknown>) => T } {
+export function asArray<T>(resolvers: (ClassOrFunctionReturning<T> | Resolver<T>)[]): { resolve: (container: AwilixContainer, opts: Record<string, unknown>) => T[] } {
 	return {
 		resolve: (container, opts) => resolvers.map((r) => container.build(r, opts)),
 	};
