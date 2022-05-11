12ef12f6f2c915981e69e2657fd372db86714b91
Adrien de Peretti <adrien.deperetti@gmail.com>
2022-05-11 09:38:12 +0200

    feat(integration-tests): Setup integration tests (#85)
    * feat(integration-tests): Setup integration tests
    
    * test(integration-tests): Update action to run integration tests
    
    * test(integration-tests): Fix pipeline
    
    * test(integration-tests): Fix tests
    
    * fix(integration-tests): Remove cwd
    
    * test(integration-tests): Update config

---
+++ integration-tests/medusa-config.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+const DB_PASSWORD = process.env.DB_PASSWORD ?? '';
+const DB_USER = process.env.DB_USER ?? 'postgres';
+const DB_HOST = process.env.DB_HOST ?? 'localhost';
+const DB_PORT = process.env.DB_PORT ?? 5432;
+
+const isCI = !!process.env.IS_CI;
+
+const DB_URL = isCI
+	? `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/medusa-extender`
+	: `postgres://${DB_USER}@${DB_HOST}:${DB_PORT}/medusa-extender`;
+
+const plugins = [
+	`medusa-fulfillment-manual`,
+	`medusa-payment-manual`,
+];
+
+module.exports = {
+	projectConfig: {
+		jwtSecret: 'supersecret',
+		cookieSecret: 'supersecret',
+
+		database_url: DB_URL,
+		database_type: 'postgres',
+	},
+	plugins
+};
---
+++ integration-tests/tests/integration.spec.ts
@@ -0,0 +1,94 @@
+import { resolve as pathResolve } from 'path';
+import express, { Express } from 'express';
+import portfinder from 'portfinder';
+import { AwilixContainer } from 'awilix';
+import { Medusa, Type } from 'medusa-extender';
+import { CartService as MedusaCartService } from '@medusajs/medusa';
+import { Server } from 'http';
+import { createDatabase, dropDatabase } from 'pg-god';
+import { CartService, TestModule, TestService } from './fixtures/components';
+import { execSync } from 'child_process';
+
+const isCI = !!process.env.IS_CI;
+
+type Context = { app: Express; appListener: Server; port: number; container: AwilixContainer };
+
+async function setupDb(): Promise<void> {
+	if (!isCI) {
+		await dropDatabase({ databaseName: 'medusa-extender' }).catch(() => void 0);
+		await createDatabase({ databaseName: 'medusa-extender' });
+	}
+	execSync('node_modules/.bin/medusa migrations run');
+}
+
+async function teardown(context: Context): Promise<void> {
+	try {
+		await new Promise((resolve, reject) =>
+			context.appListener.close((err) => (err ? reject(err) : resolve(void 0)))
+		);
+		if (!isCI) {
+			await dropDatabase({ databaseName: 'medusa-extender' });
+		}
+	} catch (e) {}
+}
+
+async function loadServer(modules: Type[]): Promise<Context> {
+	return await new Promise((resolve, reject) => {
+		portfinder.getPort(async (err: unknown, port: number) => {
+			if (err) reject(err);
+
+			const app = express();
+			const container = await new Medusa(pathResolve(__dirname, '..'), app).load(modules);
+
+			const appListener = app.listen(port);
+
+			resolve({
+				app,
+				appListener,
+				port,
+				container,
+			});
+		});
+	});
+}
+
+describe('Medusa-extender', () => {
+	let context!: Context;
+
+	beforeAll(async () => {
+		await setupDb();
+		context = await loadServer([TestModule]);
+	});
+
+	afterAll(async () => {
+		await teardown(context);
+	});
+
+	it('validate that the context is properly loaded', () => {
+		expect(context.app).toBeTruthy();
+		expect(context.appListener).toBeTruthy();
+		expect(context.port).toBeTruthy();
+		expect(context.container).toBeTruthy();
+	});
+
+	describe('services loader', () => {
+		it('should load service components', () => {
+			const testService: TestService = context.container.resolve(TestService.resolutionKey);
+			expect(testService).toBeTruthy();
+			expect(testService).toBeInstanceOf(TestService);
+		});
+
+		it('should load overridden service components', () => {
+			const cartService: CartService = context.container.resolve(CartService.resolutionKey);
+			expect(cartService).toBeTruthy();
+			expect(cartService).toBeInstanceOf(CartService);
+			expect(cartService).toBeInstanceOf(MedusaCartService);
+
+			expect(cartService.customMethod).toBeTruthy();
+
+			cartService.customMethod();
+
+			expect(cartService.customMethod).toHaveBeenCalled();
+		});
+	});
+});
