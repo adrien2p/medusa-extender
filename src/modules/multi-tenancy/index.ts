19bb13c7834af9cc6ef96abff85c56b991ddde3a
adrien2p <adrien.deperetti@gmail.com>
2022-05-24 18:46:59 +0200

    feat(multi-tenancy): Improve the module globally

--- src/modules/multi-tenancy/index.ts
+++ src/modules/multi-tenancy/index.ts
@@ -1,5 +1,2 @@
 export * from './tenant.module';
-export * from './tenant.entity';
 export * from './tenant.middleware';
-export * from './1647204670085-tenant.migration';
-export * from './tenant.repository';
