b3271c6e7838e161d036151470310ca7da34ab26
adrien2p <adrien.deperetti@gmail.com>
2022-06-17 14:40:28 +0200

    feat(multi-tenancy): Update the module to be dynamic

--- src/modules/multi-tenancy/tenant.module.ts
+++ src/modules/multi-tenancy/tenant.module.ts
@@ -1,16 +1,15 @@
 import { TenantMiddleware } from './tenant.middleware';
 import { Module } from '../../decorators';
 import { TenantService } from './tenant.service';
-import { MedusaDynamicModule, ModuleInjectionOptions } from "../../core";
-import { ConfigModule } from "./types";
+import { MedusaDynamicModule, ModuleInjectionOptions } from '../../core';
+import { ConfigModule } from './types';
 
 @Module()
 export class TenantModule implements MedusaDynamicModule {
-	async forRoot<T>(configModule: ConfigModule): Promise<ModuleInjectionOptions> {
+	async forRoot(configModule: ConfigModule): Promise<ModuleInjectionOptions> {
 		if (configModule.multi_tenancy?.enable) {
-			return { imports: [TenantMiddleware, TenantService] }
+			return { imports: [TenantMiddleware, TenantService] };
 		}
-		return { imports: [] }
+		return { imports: [] };
 	}
-
 }
