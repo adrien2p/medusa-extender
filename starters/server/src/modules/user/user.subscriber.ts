4ba5fc40072888fc0046a9ce36d87a5ed8ec1089
Juan Zapata Gomez <juanzgc@bu.edu>
2022-08-16 00:43:20 -0500

    Fix Documentation
    Discrepancy between Docs and Starter example. Docs is missing `await` in the `eventEmitter`.
    
    See https://github.com/adrien2p/medusa-extender/blob/main/starters/server/src/modules/user/user.subscriber.ts#L20

--- starters/server/src/modules/user/user.subscriber.ts
+++ starters/server/src/modules/user/user.subscriber.ts
@@ -17,7 +17,7 @@ export default class UserSubscriber implements EntitySubscriberInterface<User> {
 	 * @param event Event to pass to the event handler
 	 */
 	public async beforeInsert(event: InsertEvent<User>): Promise<void> {
-		return await eventEmitter.emitAsync(OnMedusaEntityEvent.Before.InsertEvent(User), {
+		return eventEmitter.emitAsync(OnMedusaEntityEvent.Before.InsertEvent(User), {
 			event,
 			transactionalEntityManager: event.manager,
 		});
