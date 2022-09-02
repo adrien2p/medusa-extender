6a3b67dc190aed0123e713514aa51c479b712af8
adrien2p <adrien.deperetti@gmail.com>
2022-09-02 11:59:58 +0200

    docs: Add troubleshooting section

--- starters/plugin-module/src/modules/user/user.module.ts
+++ starters/plugin-module/src/modules/user/user.module.ts
@@ -1,11 +1,10 @@
 import { Module } from 'medusa-extender';
-import { User } from './user.entity';
 import UserService from './user.service';
-import AttachUserSubscribersMiddleware from './attachUserSubscribers.middleware';
 import addStoreIdToUser1611063162649 from './20211126000001-add-storeId-to-user';
 import UserRepository from './user.repository';
+import { User } from './user.entity';
 
 @Module({
-	imports: [User, UserRepository, UserService, AttachUserSubscribersMiddleware, addStoreIdToUser1611063162649],
+	imports: [User, UserRepository, UserService, addStoreIdToUser1611063162649],
 })
 export class UserModule {}
