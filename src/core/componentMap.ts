1e013c139b6c92e61bad614fd639966c805d870a
Victor Kislichenko <v.kislichenko@gmail.com>
2022-10-06 14:06:12 +0300

    fix: when decorated and dynamic modules are loaded together presenting InjectableOptions with intersecting ComponentTypes - only entries from last one is used

--- src/core/componentMap.ts
+++ src/core/componentMap.ts
@@ -1,6 +1,18 @@
 import { GetInjectableOptions, InjectableComponentTypes } from './types';
 
 export class ComponentMap extends Map<InjectableComponentTypes, GetInjectableOptions> {
+	constructor(entries?: readonly (readonly [InjectableComponentTypes, GetInjectableOptions])[] | null) {
+		if (!entries || entries.length === 1) {
+			super(entries);
+			return;
+		}
+
+		const mergedEntries = new Map();
+		for (const [entryComponentType, entryOptions] of entries) {
+			mergedEntries.set(entryComponentType, [...(mergedEntries.get(entryComponentType) ?? []), ...entryOptions]);
+		}
+		super(mergedEntries.entries());
+	}
 	get<TComponentType extends InjectableComponentTypes>(key: TComponentType): GetInjectableOptions<TComponentType> {
 		return super.get(key) as GetInjectableOptions<TComponentType>;
 	}
