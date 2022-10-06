de06ec57ccaeab7d2ab2d0565e45435e95c96a8f
Victor Kislichenko <v.kislichenko@gmail.com>
2022-10-06 19:38:34 +0300

    fix: when decorated and dynamic modules are loaded together presenting InjectableOptions with intersecting ComponentTypes - only entries from last one is used (#133)

--- src/loaders/tests/modules.loader.spec.ts
+++ src/loaders/tests/modules.loader.spec.ts
@@ -18,6 +18,16 @@ class MyDynamicModule implements MedusaDynamicModule {
 @Service()
 class TestService2 {}
 
+@Module({ imports: [TestService2] })
+class MyModule2 {}
+
+@Module()
+class MyDynamicModule2 implements MedusaDynamicModule {
+	async forRoot(): Promise<ModuleInjectionOptions> {
+		return { imports: [TestService2] };
+	}
+}
+
 @Module()
 class MyDynamicModuleWithSubModule implements MedusaDynamicModule {
 	async forRoot(): Promise<ModuleInjectionOptions> {
@@ -46,5 +56,31 @@ describe('Modules loader', () => {
 		expect(services.length).toBe(2);
 		expect(new services[0].metatype()).toBeInstanceOf(TestService);
 		expect(new services[1].metatype()).toBeInstanceOf(TestService2);
+	});
+
+	it('should load multiple decorated modules', async () => {
+		const componentsMap = await modulesLoader([MyModule, MyModule2], {});
+		const services = componentsMap.get('service');
+		expect(services.length).toBe(2);
+		expect(new services[0].metatype()).toBeInstanceOf(TestService);
+		expect(new services[1].metatype()).toBeInstanceOf(TestService2);
+	});
+
+	it('should load multiple dynamic modules', async () => {
+		const componentsMap = await modulesLoader([MyDynamicModule, MyDynamicModule2], {});
+		const services = componentsMap.get('service');
+		expect(services.length).toBe(2);
+		expect(new services[0].metatype()).toBeInstanceOf(TestService);
+		expect(new services[1].metatype()).toBeInstanceOf(TestService2);
+	});
+
+	it('should load multiple decorated and dynamic modules', async () => {
+		const componentsMap = await modulesLoader([MyModule, MyDynamicModule2], {});
+		const services = componentsMap.get('service');
+		expect(services.length).toBe(2);
+
+		/* Note: dynamic modules are always first to be added to ComponentMap */
+		expect(new services[0].metatype()).toBeInstanceOf(TestService2);
+		expect(new services[1].metatype()).toBeInstanceOf(TestService);
 	});
 });
