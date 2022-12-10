b9030c48335ae682f28cceeb25d806e8dab43c84
SGF <SGF@sourcegoodfood.com>
2022-12-10 08:15:06 +0000

    fix: linting issues

--- src/cli/utils/fixtures/async-parameter.js
+++ src/cli/utils/fixtures/async-parameter.js
@@ -4,15 +4,11 @@
 		}  
 module.exports = {
 			projectConfig: {
-			// redis_url: REDIS_URL,
-			// For more production-like environment install PostgresQL
-			// database_url: DATABASE_URL,
-			// database_type: "postgres",
 			database_type: "sqlite",
 			database_database: "./medusa-db.sql",
 			admin_cors: "ADMIN_CORS",
 			store_cors: "STORE_CORS",
 			database_password:password
 		  },
-		  plugins:[],
+		  	plugins:[],
 		};
