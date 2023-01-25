3e0b2ad15ea8df549dd604e3a4823096241c00ae
Saboor Hakimi <44523544+Saboor-Hakimi@users.noreply.github.com>
2023-01-25 09:56:01 +0430

    comment about { method: 'all', path: '*' }

--- src/cli/templates/middleware-template.ts
+++ src/cli/templates/middleware-template.ts
@@ -13,6 +13,9 @@ export function getMiddlewareTemplate(middlewareName: string): string {
         } from 'medusa-extender';
         import { NextFunction, Response } from 'express';
         
+	// Note: When applying this middleware to the wildcard (*) route, it will act as a custom domain middleware.
+	// This means that the requiredAuth middleware will be applied and the middleware will be loaded before any routes or middleware in the /admin or /store directories.
+	// Keep in mind that /admin and /store routes have their own loader.
         @Middleware({ requireAuth: true, routes: [{ method: 'all', path: '*' }] })
         export class ${middlewareName} implements MedusaMiddleware {
             public consume(req: MedusaAuthenticatedRequest, res: Response, next: NextFunction): void | Promise<void> {
