7dae9392c310ff36877ecb77b5793d09bcc24856
Saboor Hakimi <44523544+Saboor-Hakimi@users.noreply.github.com>
2023-01-27 11:04:02 +0430

    removed the unnecessary indentations in the template code for comments

--- src/cli/templates/middleware-template.ts
+++ src/cli/templates/middleware-template.ts
@@ -13,9 +13,10 @@ export function getMiddlewareTemplate(middlewareName: string): string {
         } from 'medusa-extender';
         import { NextFunction, Response } from 'express';
         
-	// Note: When applying this middleware to the wildcard (*) route, it will act as a custom domain middleware.
-	// This means that the requiredAuth middleware will be applied and the middleware will be loaded before any routes or middleware in the /admin or /store directories.
-	// Keep in mind that /admin and /store routes have their own loader.
+// Note: When applying this middleware to the wildcard (*) route, it will act as a custom domain middleware.
+// This means that if the `requiredAuth` options is applied the admin auth middleware is being choose to be applied. Custom middlewares will be loaded before any routes or middleware in the /admin or /store directories.
+// To specifically act on the admin or store domain you can use `/admin/*` or `/store/*`
+
         @Middleware({ requireAuth: true, routes: [{ method: 'all', path: '*' }] })
         export class ${middlewareName} implements MedusaMiddleware {
             public consume(req: MedusaAuthenticatedRequest, res: Response, next: NextFunction): void | Promise<void> {
